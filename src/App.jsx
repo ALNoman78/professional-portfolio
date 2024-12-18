31d3b591683ce325f109815d5ce1f7417441f222
ALNoman78 <abdullaalno078@gmail.com>
2024-12-18 23:25:39 +0600

    contact form and project setup completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Contact from './componentes/ContactForm/Contact'
+
+function App() {
+
+  return (
+    <>
+      <Contact></Contact>
+    </>
+  )
+}
+
+export default App
