44d28cf4942fc0cb76d0dbdc2124d78e3596c85d
ALNoman78 <abdullaalno078@gmail.com>
2024-12-19 11:28:42 +0600

    contact form change

--- src/componentes/ContactForm/Contact.jsx
+++ src/componentes/ContactForm/Contact.jsx
@@ -1,16 +1,22 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import { Field, Label, Switch } from '@headlessui/react'
+import { Link } from 'react-router-dom';
 
 export default function Example() {
     const [agreed, setAgreed] = useState(false);
-    const [email , setEmail] = useState(null)
+    const [email, setEmail] = useState(null)
+    const firstNameRef = useRef(null)
 
     const handleEmail = e => {
         setEmail(e.target.value)
     }
+
+    useEffect(() => {
+        firstNameRef.current.focus()
+    }, [])
 
     const handleFormSubmit = e => {
         e.preventDefault();
@@ -31,7 +37,7 @@ export default function Example() {
                 />
             </div>
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Contact sales</h2>
+                <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Contact Me</h2>
                 <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
             </div>
             <form action="#" method="POST" onSubmit={handleFormSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
@@ -41,7 +47,7 @@ export default function Example() {
                             First name
                         </label>
                         <div className="mt-2.5">
-                            <input
+                            <input ref={firstNameRef}
                                 id="first-name"
                                 name="first-name"
                                 type="text"
@@ -74,7 +80,7 @@ export default function Example() {
                                 name="company"
                                 type="text"
                                 autoComplete="organization"
-                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" required
                             />
                         </div>
                     </div>
@@ -88,7 +94,7 @@ export default function Example() {
                                 name="email"
                                 type="email"
                                 autoComplete="email"
-                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" required
                             />
                         </div>
                     </div>
@@ -123,7 +129,7 @@ export default function Example() {
                                     name="phone-number"
                                     type="text"
                                     placeholder="123-456-7890"
-                                    className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 bg-white"
+                                    className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 bg-white" required
                                 />
                             </div>
                         </div>
@@ -161,15 +167,13 @@ export default function Example() {
                             <a href="#" className="font-semibold text-indigo-600">
                                 privacy&nbsp;policy
                             </a>
-                            .
                         </Label>
                     </Field>
                 </div>
                 <div className="mt-10">
                     <button
                         type="submit"
-                        className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
+                        className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         Let's talk
                     </button>
                 </div>
---
+++ src/componentes/ContactSubmit/ContactSubmit.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ContactSubmit = props => {
+    return (
+        <div>ContactSubmit</div>
+    )
+}
+
+ContactSubmit.propTypes = {}
+
+export default ContactSubmit
