d281e86ee77071a53e8eb82986a3973c784070d3
ALNoman78 <abdullaalno078@gmail.com>
2025-01-18 23:18:42 +0600

    framer motion added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,7 @@
-import Index from '../NewBanner/Index'
-
 const Projects = () => {
     return (
         <div>
             <h2>Projects</h2>
-            {/* <Index></Index> */}
         </div>
     )
 }
