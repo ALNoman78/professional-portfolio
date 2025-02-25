82dec4698e07c5b7bc2838fca0f78fd8b21701a5
ALNoman78 <abdullaalno078@gmail.com>
2025-02-26 02:11:08 +0600

    all bug fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,4 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-const Footer = props => {
+const Footer = () => {
     return (
         <footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
             <nav className="grid grid-flow-col gap-4">
